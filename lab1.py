09e1c3b38911c3259d3d91c6a75ac41d7b6e0b89
SHIKHAR458 <shikharp108@gmail.com>
2025-07-31 15:34:27 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,13 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+mark = int(input())
+if 90 <= mark and mark <= 100:
+    print("A")
+elif 75 <= mark and mark <= 89:
+    print("B")
+elif 60 <= mark and mark <= 74:
+    print("C")
+elif 40 <= mark and mark <= 59:
+    print("D")
+elif 0 <= mark and mark <= 39:
+    print("F")
+else:
+    print("Invalid mark")
